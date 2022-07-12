881bc9395f2539d1d2de0ec7e9c459a17b0a9f5e
renatojobal <renatojobal@gmail.com>
2022-07-12 16:36:15 -0500

    :lipstick: better style for pedidos page

--- internas/pedidos.php
+++ internas/pedidos.php
@@ -1,64 +1,72 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Mi primera app</title>
-	<link rel="stylesheet" type="text/css" href="../css/estilos.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mi primera app</title>
+    <link rel="stylesheet" type="text/css" href="../css/pedidos.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-	<header class="cabeceraPrincipal">
-		<h1>Vilonaco Food</h1>
-	</header>
-	<nav class="menuPrincipal">
-		<a href="../">Inicio</a>
-		<a href="">Empresa</a>
-		<a href="contactos.php">Registro</a>
-		<a href="listados.php">Listado</a>
-		<a href="login.php">Login</a>
+    <header class="cabeceraPrincipal">
+        <h1>Vilonaco Food</h1>
+    </header>
+    <nav class="menuPrincipal">
+        <a href="../">Inicio</a>
+        <a href="">Empresa</a>
+        <a href="contactos.php">Registro</a>
+        <a href="listados.php">Listado</a>
+        <a href="login.php">Login</a>
         <a href="pedidos.php">Pedidos</a>
-	</nav>
-	
-	<main>
-		<div class="boxCentrado">
-		<h2 class="h2Home">Hacer un pedido</h2>
-		<form>
-        <div class="form-group">
-            <label for="nombre">Nombre del comensal</label>
-            <input type="text" class="form-control" id="nombre" placeholder="Jhon Doe">
-                  </div>
-        <div class="form-group">
-        <div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-  <label class="form-check-label" for="exampleRadios1">
-    Default radio
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-  <label class="form-check-label" for="exampleRadios2">
-    Second default radio
-  </label>
-</div>
-<div class="form-check disabled">
-  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" disabled>
-  <label class="form-check-label" for="exampleRadios3">
-    Disabled radio
-  </label>
-</div>
+    </nav>
+
+    <main>
+        <div class="boxCentrado">
+            <h2 class="h2Home">Hacer un pedido</h2>
+            <form>
+                <div class="form-group">
+                    <label for="nombre">Nombre del comensal</label>
+                    <input type="text" class="form-control" id="nombre" placeholder="Jhon Doe">
+                </div>
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1"
+                            value="option1">
+                        <label class="form-check-label" for="exampleRadios1">
+                            Default radio
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2"
+                            value="option2">
+                        <label class="form-check-label" for="exampleRadios2">
+                            Second default radio
+                        </label>
+                    </div>
+                    <div class="form-check disabled">
+                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3"
+                            value="option3" disabled>
+                        <label class="form-check-label" for="exampleRadios3">
+                            Disabled radio
+                        </label>
+                    </div>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-		</div>
-	</main>
-	<footer class="piePagina"><h6>Derechos Reservados 2022</h6></footer>
+    </main>
+    <footer class="piePagina">
+        <h6>Derechos Reservados 2022</h6>
+    </footer>
 </body>
+
 </html>
